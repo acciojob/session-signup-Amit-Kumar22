db118797c8b858088b823a3d01aa670ccfc0408d
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-24 11:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const btn=document.getElementById("submit");
+const prints=document.getElementById("session-storage");
+btn.addEventListener("click", ()=>{
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+    if (password != confirmPassword) {
+        alert("Passwords do not match");
+		prints.style.display = "block";
+        return false;
+    }
+	else{
+		const name=document.getElementById("name").value;
+		const email=document.getElementById("email").value;
+		const profileInfo = `
+            <h2>Profile Details</h2>
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>password:</strong> ${password}</p>
+        `;
+        prints.innerHTML = profileInfo;
+		 alert("Sign up successful!");
+		return true;
+	}
+    
+})
